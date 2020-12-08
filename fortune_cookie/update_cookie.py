74b27ca319e6e4c6fb2d347bf0967fd6a614ab8a
Donato Rimenti <donatohan.rimenti@gmail.com>
2020-12-08 22:57:20 +0100

    Updated cookie message

--- fortune_cookie/update_cookie.py
+++ fortune_cookie/update_cookie.py
@@ -1,1 +1,33 @@
+import random
+import time
+from git import Repo
+
+# Opens the fortunes file and reads a random line
+with open('./fortunes.cookie', encoding='utf-8') as f:
+    lines = f.readlines()
+
+random_line_number = random.randint(2, len(lines) - 1)
+fortune = lines[random_line_number]
+print('Today fortune is: ' + fortune)
+
+# Replace the fortune cookie sentence in the README
+with open('../README.md', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+for i, line in enumerate(lines):
+    if line.startswith('> '):
+        lines[i] = '> 🥠 ' + fortune
+    elif line.startswith('Last update:'):
+        lines[i] = 'Last update: ' + time.ctime()
+
+with open('../README.md', 'w', encoding='utf-8') as f:
+    f.writelines(lines)
+
+# Commits the file
+repo = Repo("..")
+repo.git.add(update=True)
+repo.index.commit("Updated cookie message")
+origin = repo.remote(name='origin')
+origin.push()
+
 print("Updating")
