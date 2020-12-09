d798e4f06225b398d7b955c0986238b52cb2eea0
Donato Rimenti <donatohan.rimenti@gmail.com>
2020-12-09 07:58:33 +0100

    Update update_cookie.py

--- fortune_cookie/update_cookie.py
+++ fortune_cookie/update_cookie.py
@@ -4,7 +4,7 @@ from git import Repo
 import os
 
 # Opens the fortunes file and reads a random line
-with open('./fortunes.cookie', encoding='utf-8') as f:
+with open('./fortune_cookie/fortunes.cookie', encoding='utf-8') as f:
     lines = f.readlines()
 
 random_line_number = random.randint(2, len(lines) - 1)
@@ -12,7 +12,7 @@ fortune = lines[random_line_number]
 print('Today fortune is: ' + fortune)
 
 # Replace the fortune cookie sentence in the README
-with open('../README.md', 'r', encoding='utf-8') as f:
+with open('./README.md', 'r', encoding='utf-8') as f:
     lines = f.readlines()
 
 for i, line in enumerate(lines):
@@ -21,12 +21,12 @@ for i, line in enumerate(lines):
     elif line.startswith('Last update:'):
         lines[i] = 'Last update: ' + time.ctime()
 
-with open('../README.md', 'w', encoding='utf-8') as f:
+with open('./README.md', 'w', encoding='utf-8') as f:
     f.writelines(lines)
 
 # Commits the updated files
 remote_url = os.getenv('REMOTE_URL')
-repo = Repo("..")
+repo = Repo(".")
 repo.git.add(update=True)
 repo.index.commit("Updated fortune cookie message")
 origin = repo.remote(name='origin')
